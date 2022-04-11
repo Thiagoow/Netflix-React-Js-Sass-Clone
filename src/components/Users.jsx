3736c7a1315408d33c2e3f797e7ada0c439d2c56
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2022-04-11 15:03:01 -0300

    Images changed from 'assets' to 'public' folder

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,27 +1,27 @@
 const users = [
   {
     id: 1,
-    photo: "/assets/img/profiles/user1.png",
+    photo: "/images/profiles/user1.png",
     name: "Thiago"
   },
   {
     id: 1,
-    photo: "/assets/img/profiles/user2.png",
+    photo: "/images/profiles/user2.png",
     name: "Lucas"
   },
   {
     id: 1,
-    photo: "/assets/img/profiles/user3.png",
+    photo: "/images/profiles/user3.png",
     name: "Lulee"
   },
   {
     id: 1,
-    photo: "/assets/img/profiles/user4.png",
+    photo: "/images/profiles/user4.png",
     name: "Mih"
   },
   {
     id: 1,
-    photo: "/assets/img/profiles/user5.png",
+    photo: "/images/profiles/user5.png",
     name: "Infantil"
   }
 ];
