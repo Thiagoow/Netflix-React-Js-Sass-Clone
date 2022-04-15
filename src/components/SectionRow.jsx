0f3b1fdc2dcdac7bda71082cffb575a65044d51a
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2022-04-15 00:18:44 -0300

    className changed

--- src/components/SectionRow.jsx
+++ src/components/SectionRow.jsx
@@ -9,11 +9,8 @@ export default function SectionRow({ title, items }) {
         {/* If there are results (> 0), render: */}
         {items.results.length > 0 &&
           items.results.map((item, key) => (
-            <div key={key} className="section">
-              <img
-                width="80px"
-                src={`https://image.tmdb.org/t/p/w200${item.poster_path}`}
-              />
+            <div key={key} className="item">
+              <img src={`https://image.tmdb.org/t/p/w200${item.poster_path}`} />
             </div>
           ))}
       </div>
