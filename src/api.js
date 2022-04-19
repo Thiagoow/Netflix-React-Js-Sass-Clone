74e40ef9e19b2126629c9ae94572d65e4eb35846
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2022-04-19 17:49:42 -0300

    Action movies commented + order changed

--- src/api.js
+++ src/api.js
@@ -23,17 +23,24 @@ export default {
         )
       },
       {
-        slug: "trending",
-        title: "Em alta",
-        items: await APIfetch(
-          `/trending/all/week?api_key=${APIkey}&with_networks=213`
-        )
-      },
-      {
         slug: "topRated",
         title: "Recomendados para você",
         items: await APIfetch(
           `/tv/top_rated?api_key=${APIkey}&with_networks=213`
+        )
+      },
+      {
+        slug: "documentaries",
+        title: "Documentários",
+        items: await APIfetch(
+          `/discover/tv?api_key=${APIkey}&with_networks=213&with_genres=99`
+        )
+      },
+      {
+        slug: "trending",
+        title: "Em alta",
+        items: await APIfetch(
+          `/trending/all/week?api_key=${APIkey}&with_networks=213`
         )
       },
       {
@@ -43,13 +50,16 @@ export default {
           `/discover/movie?api_key=${APIkey}&with_networks=213&with_genres=16`
         )
       },
+
+      /*
       {
         slug: "action",
         title: "Ação e aventura",
         items: await APIfetch(
           `/discover/tv?api_key=${APIkey}&with_networks=213&with_genres=10759`
         )
-      },
+      }, 
+      */
       {
         slug: "romance",
         title: "Histórias de amor",
@@ -62,13 +72,6 @@ export default {
         title: "De roer as unhas",
         items: await APIfetch(
           `/discover/movie?api_key=${APIkey}&with_networks=213&with_genres=27`
-        )
-      },
-      {
-        slug: "documentaries",
-        title: "Documentários",
-        items: await APIfetch(
-          `/discover/tv?api_key=${APIkey}&with_networks=213&with_genres=99`
         )
       }
     ];
