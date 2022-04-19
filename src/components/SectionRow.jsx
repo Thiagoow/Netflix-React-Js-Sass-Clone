b13ed677dccac519bafb21a96c865743cc70d3e5
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2022-04-19 17:06:19 -0300

    Home content appearing in the right way :D

--- src/components/SectionRow.jsx
+++ src/components/SectionRow.jsx
@@ -2,18 +2,22 @@ import React from "react";
 
 export default function SectionRow({ title, items }) {
   return (
-    <>
-      <h2>{title}</h2>
+    <div className="movieRow">
+      <h2 className="title">{title}</h2>
 
-      <div className="movieRow listArea">
+      <div className="section">
         {/* If there are results (> 0), render: */}
-        {items.results.length > 0 &&
-          items.results.map((item, key) => (
-            <div key={key} className="item">
-              <img src={`https://image.tmdb.org/t/p/w200${item.poster_path}`} />
-            </div>
-          ))}
+        <div className="content">
+          {items.results.length > 0 &&
+            items.results.map((item, key) => (
+              <div key={key} className="item">
+                <img
+                  src={`https://image.tmdb.org/t/p/w200${item.poster_path}`}
+                />
+              </div>
+            ))}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
