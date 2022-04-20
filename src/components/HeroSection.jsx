5360af3a7b0355b8798dff7f398c9469d1ee309f
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-04-20 19:30:36 -0300

    Getting genres name :D

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 
 export default function HeroSection({ media }) {
-  return (
-    <section className="heroSection">
-      <h2>Netflix Hero</h2>
+  let mediaBg = `https://image.tmdb.org/t/p/original${media.poster_path}`;
+  let mediaYear = new Date(media.first_air_date).getFullYear();
+  //TODO: Get genres names:
+  let genres = [];
 
-      <div className="mediaContent">
-        <p>{media.name}</p>
+  return (
+    <section
+      className="heroSection"
+      style={{
+        backgroundImage: `url(${mediaBg})`
+      }}
+    >
+      <div className="rightGradient">
+        <div className="mediaContent">
+          <div className="info">
+            <h1 className="title">{media.name}</h1>
+
+            <b className="row">
+              <p className="points">{media.vote_average} pontos</p>
+              <p className="year">{mediaYear}</p>
+              <p className="seasons">
+                {media.number_of_seasons} temporada
+                {/*👇🏽 If it's plural add 's', if not add nothing */}
+                {media.number_of_seasons !== 1 ? "s" : ""}
+              </p>
+            </b>
+
+            <p className="desc">{media.overview}</p>
+
+            <p className="genres">
+              <b>Gêneros:</b>
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
