b8393994121a2839b5443d92c3f236b217939433
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-04-22 19:39:08 -0300

    fetchOnlyFavorites working :DD ✔

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,53 +7,48 @@ const Home = () => {
   const [mediaList, setMediaList] = React.useState([]);
   const [HeroData, setHeroData] = React.useState(null);
 
-  React.useEffect(() => {
-    const loadSections = async () => {
-      let list = await api.getHomeList();
-      //console.log(list);
-      setMediaList(list);
-    };
+  async function loadSections() {
+    let list = await api.getHomeList();
+    console.log(list);
+    setMediaList(list);
+  }
 
-    async function fetchOnlyFavorites(item) {
-      const favoriteMedias = [61889, 94605, 93405, 114868];
-      let medias = [];
+  async function loadFavoriteHero(item) {
+    const favoriteMedias = [
+      61889, 94605, 93405, 114868, 77169, 66732, 103786, 71912, 890
+    ];
+    let medias = [];
 
-      for (let i = 0; i < favoriteMedias.length; i++) {
-        item = await api.getMediaInfo(favoriteMedias[i], "tv");
-        medias.push(item);
-      }
-      let randomMedia = Math.floor(
-        //Catches a random array position (-1 because array starts in 0):
-        Math.random() * (medias.length - 1)
-      );
-
-      item = medias[randomMedia];
-      console.log(item);
-
-      return item;
+    for (let i = 0; i < favoriteMedias.length; i++) {
+      item = await api.getMediaInfo(favoriteMedias[i], "tv");
+      medias.push(item);
     }
+    let randomMedia = Math.floor(Math.random() * (medias.length - 1));
 
-    const loadHero = async () => {
-      let list = await api.getHomeList();
-      let filterBySlug = list.filter((i) => i.slug === "originals");
-      /**/
-      let filterMedia = Math.floor(
-        //Catches a random array position (-1 because array starts in 0):
-        Math.random() * (filterBySlug[0].items.results.length - 1)
-      );
-      let media = filterBySlug[0].items.results[filterMedia];
-      //Catches additional info about the filterMedia:
-      let hero = await api.getMediaInfo(media.id, "tv");
+    item = medias[randomMedia];
+    //console.log(item);
+    return item;
+  }
+  async function loadHero() {
+    let list = await api.getHomeList();
+    let filterBySlug = list.filter((i) => i.slug === "originals");
+    /* 
+    let filterMedia = Math.floor(
+      //Catches a random array position (-1 because array starts in 0):
+      Math.random() * (filterBySlug[0].items.results.length - 1)
+    );
+    let media = filterBySlug[0].items.results[filterMedia];
+    //Catches additional info about the filterMedia:
+    let hero = await api.getMediaInfo(media.id, "tv");*/
 
-      /*Fetching only favoriteMedias:
-      let hero = await fetchOnlyFavorites(filterBySlug);
-      */
+    /*Fetching only favoriteMedias: */
+    let hero = await loadFavoriteHero(filterBySlug);
 
-      //console.log(hero);
-      setHeroData(hero);
-    };
+    setHeroData(hero);
+  }
 
-    //Executa as funções:
+  //==== Executa as funções:
+  React.useEffect(() => {
     loadSections();
     loadHero();
   }, []);
