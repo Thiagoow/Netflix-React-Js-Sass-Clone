49af08fda73e68d45b2a424fccdd712c2d85c36e
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-04-24 18:09:29 -0300

    Header almost done :D

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import logo from "../../assets/img/logo.svg";
+import sass from "../../assets/scss/base/_export.module.scss";
 
 const Header = () => {
+  const [scrollColor, setScrollColor] = React.useState(false);
+
+  const changeHeaderColor = () => {
+    //👇🏽 54px = Header height
+    if (window.scrollY >= 54) {
+      setScrollColor(true);
+    } else {
+      setScrollColor(false);
+    }
+  };
+
+  //Activate the function above:
+  React.useEffect(() => {
+    window.addEventListener("scroll", changeHeaderColor);
+  }, []);
+
   return (
-    <header className="header">
-      <p>Hi there, i'm a header :D</p>
+    <header
+      className="header"
+      style={{
+        background: scrollColor ? sass.bodyColor : "transparent"
+      }}
+    >
+      <a href="/" className="logoBtn">
+        <img src={logo} alt="Netflix Logo" className="logo" />
+      </a>
     </header>
   );
 };
